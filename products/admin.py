81ec3d9f76fb7c642ecd27db87baaab22f19b3d8
roasteRKopsus <azka.mhmmd@gmail.com>
2020-07-20 22:42:56 +0700

    add total roastery

--- products/admin.py
+++ products/admin.py
@@ -17,42 +17,7 @@ class QCSampleBeanAdmin(ExportActionMixin, admin.ModelAdmin):
 class BeansGudangAdmin(ExportActionMixin, admin.ModelAdmin):
 	list_display = ('sample_code','biji','vendor_name', 'lot_number', 'bag_amount','berat_kopi_in_kg', 'qc_acceptance')
 
-
-
-
-
-class TotalAveragesChangeList(ChangeList):
-	fields_to_total = ['berat_akhir','berat_masuk', 'persentase_susut']
-
-	def get_total_values(self, queryset):
-		total =  Roaster()
-		total.custom_alias_name = "Totals"
-		for field in self.fields_to_total:
-			setattr(total, field, queryset.aggregate(Sum(field)).items()[0][1])
-		return total
-
-	def get_average_values(self, queryset):
-		average =  Roaster()
-		average.custom_alias_name = "Averages"
-		for field in self.fields_to_total:
-			setattr(average, field, queryset.aggregate(Avg(field)).items()[0][1])
-		return average
-
-	def get_results(self, request):
-		super(TotalAveragesChangeList, self).get_results(request)
-		total = self.get_total_values(self.query_set)
-		average = self.get_average_values(self.query_set)
-		len(self.result_list)
-		self.result_list._result_cache.append(total)
-		self.result_list._result_cache.append(average)
-
-
-
 class RoasterAdmin(ExportActionMixin, admin.ModelAdmin):
-
-	def get_changelist(self, request, **kwargs):
-		return TotalAveragesChangeList
-
 	list_display = ('roast_date',
 'beans_name',
 'mesin',
@@ -68,10 +33,8 @@ class RoasterAdmin(ExportActionMixin, admin.ModelAdmin):
 	
 	list_filter=(('roast_date', PastDateRangeFilter),'mesin','roaster_pass_check', ('beans_name', RelatedDropdownFilter))
 	# prepopulated_fields = {'susut':('persentase_susut')}
-	#change_list_template = None
-	#total = Roaster.objects.aggregate(Sum( 'berat_akhir'))
-
-	
+	change_list_template = None
+	total = Roaster.objects.aggregate(Sum( 'berat_akhir'))
 
 
 admin.site.register(BeansGudang, BeansGudangAdmin)
